e95771c0a705c7ee4af4aad2dddd359acc87fc0e
Sinzera <sinzera@gmail.com>
2019-02-20 12:57:31 +0700

    Lesson 2

--- js/script.js
+++ js/script.js
@@ -32,14 +32,80 @@ window.addEventListener('DOMContentLoaded', () => {
 
             trigger.remove();
 
+            showConfirm();
+
             removeBtn.classList.add('goods__item-remove');
             removeBtn.innerHTML = '&times';
             item.appendChild(removeBtn);
 
             cartWrapper.appendChild(item);
             if (empty) {
-                empty.remove();
+                empty.style.display = 'none';
             }
+
+            calcGoods();
+            calcTotal();
+            removeFromCart();
         });
     });
+
+
+    function sliceTitle() {
+        titles.forEach(function (item) {
+            if (item.textContent.length < 70) {
+                return;
+            } else {
+                const str = `${item.textContent.slice(0, 70)}...`;
+                item.textContent = str;
+            }
+        });
+    }
+
+    sliceTitle();
+
+    function showConfirm() {
+        confirm.style.display = 'block';
+        let counter = 100;
+        const id = setInterval(frame, 10);
+        function frame() {
+            if (counter == 10) {
+                clearInterval(id);
+                confirm.style.display = 'none';
+            } else {
+                counter--;
+                confirm.style.transform = `translateY(-${counter}px)`;
+                confirm.style.opacity = '.' + counter;
+            }
+        }
+    }
+
+    function calcGoods() {
+        const items = cartWrapper.querySelectorAll('.goods__item');
+        badge.textContent = items.length;
+    }
+
+    function calcTotal() {
+        const prices = cartWrapper.querySelectorAll('.goods__item > .goods__price > span')
+        let total = 0;
+        prices.forEach(function (item) {
+            total += +item.textContent;
+        });
+        totalCost.textContent = total;
+    }
+
+    function removeFromCart() {
+        const removeBtn = cartWrapper.querySelectorAll('.goods__item-remove');
+        removeBtn.forEach(function (btn) {
+            btn.addEventListener('click', () => {
+                btn.parentElement.remove();
+                calcGoods();
+                calcTotal();
+
+                if (cartWrapper.querySelectorAll('.goods__item').length == 0) {
+                    const empty = cartWrapper.querySelector('.empty');
+                    empty.style.display = 'block';
+                }
+            });
+        })
+    }
 });
